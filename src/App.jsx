7381f70681571502ea31f3a356bc99edbf3c75dc
Nimira43 <n.rabone72@aol.com>
2025-09-12 14:39:38 +0100

    Showing price and price change percentage

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,20 @@ const App = () => {
                   className='coin-image'
                 />
                 <div>
-                  <h2>{coin.name}</h2>
+                  <h2 className='main-colour'>{coin.name}</h2>
                   <p className='symbol'>{coin.symbol.toUpperCase()}</p>
                 </div>
               </div>
+              <p>Price: £{coin.current_price.toLocaleString()}</p>
+              <p 
+                className={ 
+                  coin.price_change_percentage_24h >= 0
+                  ? 'positive'
+                  : 'negative'
+                }
+              >
+                {coin.price_change_percentage_24h}
+              </p>
             </div>
           ))}
         </main>
