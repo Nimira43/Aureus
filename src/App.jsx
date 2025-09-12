fe55e9f1818fdc90348cdd0eed72a29e906b538e
Nimira43 <n.rabone72@aol.com>
2025-09-12 12:57:20 +0100

    Created error state variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react'
 const App = () => {
   const [coins, setCoins ] = useState([])
   const [loading, setLoading] = useState(true)
-   
+  const [error, setError] = useState(null)
+
 
   return ( 
     <div>
