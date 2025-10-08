d252b60560e0870fa105ba3138b7c88469c7643a
Nimira43 <n.rabone72@aol.com>
2025-10-08 15:34:38 +0100

    Moved controls markup to LimitSelector

--- src/App.jsx
+++ src/App.jsx
@@ -33,26 +33,7 @@ const App = () => {
           <GrMoney className='logo-icon' />
           <h1 className='logo-text'>Aureus</h1>
         </div> 
-        <div className='controls'>
-          <label
-            htmlFor='limit'
-          >
-            Show
-          </label>
-          <select 
-            value={limit} 
-            id='limit'
-            onChange={
-              (e) => setLimit(Number(e.target.value))
-            }
-          >
-            <option value='5'>5</option>
-            <option value='10'>10</option>
-            <option value='20'>20</option>
-            <option value='50'>50</option>
-            <option value='100'>100</option>
-          </select>
-        </div>
+        
       </div>
       { loading && 
         <p>Loading...</p>
--- src/components/LimitSelector.jsx
+++ src/components/LimitSelector.jsx
@@ -1,6 +1,25 @@
 const LimitSelector = ({limit, onLimitChange}) => {
   return ( 
-    <div>LimitSelector</div>
+    <div className='controls'>
+      <label
+        htmlFor='limit'
+      >
+        Show
+      </label>
+      <select 
+        value={limit} 
+        id='limit'
+        onChange={
+          (e) => onLimitChange(Number(e.target.value))
+        }
+      >
+        <option value='5'>5</option>
+        <option value='10'>10</option>
+        <option value='20'>20</option>
+        <option value='50'>50</option>
+        <option value='100'>100</option>
+      </select>
+    </div>
    )
 }
  
